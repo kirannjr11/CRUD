dd92277c268d7b14600c57b83bb979f0cf6b6c3d
kiran <ghalekiran03@gmail.com>
2024-05-15 03:07:16 +0900

    updated

--- src/main/java/habsida/spring/boot_security/demo/repositories/RoleRepository.java
+++ src/main/java/habsida/spring/boot_security/demo/repositories/RoleRepository.java
@@ -3,7 +3,9 @@ package habsida.spring.boot_security.demo.repositories;
 import habsida.spring.boot_security.demo.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
