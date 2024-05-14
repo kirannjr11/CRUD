b8cc99c1d0a772fd37dca7988def73fb45b3c35c
kiran <ghalekiran03@gmail.com>
2024-05-15 03:05:32 +0900

    updated

--- src/main/java/habsida/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/habsida/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,8 +2,10 @@ package habsida.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
